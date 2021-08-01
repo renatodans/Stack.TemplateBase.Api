715760ada7541fbc0cf15876e3bdd55254231890
Renato Dans Dias <renato.dans@elevenfinancial.com>
2021-08-01 18:21:26 -0300

    Initial commit

---
+++ src/Stack.TemplateBase.Api/Controllers/StackController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack.TemplateBase.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class  StackController : ControllerBase
+    {
+        private static readonly string[] Stacks = new[]
+        {
+            "C#", "Visual Studio", "VSCode", "Azure", "DevOps", "React Native", "React", "JS", "Node"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Stacks.ToArray();
+        }
+    }
+}
